024f70b9e76f6533be9b07558a791171c3a2f6a5
Noah Tye <noah@twitter.com>
2012-04-03 08:23:31 -0700

    remove old linux configuration; add osx keyboard config

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Setup custom keyboard.
+
+cp ./NoahDvorak.keylayout ~/Library/Keyboard\ Layouts/NoahDvorak.keylayout
+curl -o ~/Downloads/KeyRemap4MacBook-7.5.0.pkg.zip http://pqrs.org/macosx/keyremap4macbook/files/KeyRemap4MacBook-7.5.0.pkg.zip
+pushd ~/Downloads
+unzip KeyRemap4MacBook-7.5.0.pkg.zip
+sudo installer -pkg ./KeyRemap4MacBook.pkg -target /
+popd
+/Library/org.pqrs/KeyRemap4MacBook/app/KeyRemap4MacBook_cli.app/Contents/MacOS/KeyRemap4MacBook_cli enable remap.commandR2optionR
