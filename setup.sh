dbe419d4d0c25b23f8e825677820cda3a3e2e6fd
Noah Tye <noah@twitter.com>
2012-04-03 08:34:36 -0700

    in setup.sh, check to see whether KeyRemap4MacBook is already installed

--- setup.sh
+++ setup.sh
@@ -3,9 +3,12 @@
 # Setup custom keyboard.
 
 cp ./NoahDvorak.keylayout ~/Library/Keyboard\ Layouts/NoahDvorak.keylayout
-curl -o ~/Downloads/KeyRemap4MacBook-7.5.0.pkg.zip http://pqrs.org/macosx/keyremap4macbook/files/KeyRemap4MacBook-7.5.0.pkg.zip
-pushd ~/Downloads
-unzip KeyRemap4MacBook-7.5.0.pkg.zip
-sudo installer -pkg ./KeyRemap4MacBook.pkg -target /
-popd
+if [ ! -d /Library/org.pqrs/KeyRemap4MacBook/ ]
+then
+	curl -o ~/Downloads/KeyRemap4MacBook-7.5.0.pkg.zip http://pqrs.org/macosx/keyremap4macbook/files/KeyRemap4MacBook-7.5.0.pkg.zip
+	pushd ~/Downloads
+	unzip KeyRemap4MacBook-7.5.0.pkg.zip
+	sudo installer -pkg ./KeyRemap4MacBook.pkg -target /
+	popd
+fi
 /Library/org.pqrs/KeyRemap4MacBook/app/KeyRemap4MacBook_cli.app/Contents/MacOS/KeyRemap4MacBook_cli enable remap.commandR2optionR
