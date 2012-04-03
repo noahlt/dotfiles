586d1097b90f41c05925a1b3af43176ab35148c3
Noah Tye <noah@twitter.com>
2012-04-03 10:37:12 -0700

    swap command and option, add more help text

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo "Show Library directory in Finder"
+chflags nohidden ~/Library/
+
 # Setup custom keyboard.
 
 echo "Copying NoahDvorak to ~/Library/Keyboard Layouts"
@@ -8,3 +11,10 @@ cp ./NoahDvorak.keylayout ~/Library/Keyboard\ Layouts/NoahDvorak.keylayout
 echo "Swapping Command_R and Option_R"
 /Library/org.pqrs/KeyRemap4MacBook/app/KeyRemap4MacBook_cli.app/Contents/MacOS/KeyRemap4MacBook_cli enable remap.commandR2optionR
 /Library/org.pqrs/KeyRemap4MacBook/app/KeyRemap4MacBook_cli.app/Contents/MacOS/KeyRemap4MacBook_cli enable remap.optionrcommandr
+
+echo "###########################################################################"
+echo "#                                                                         #"
+echo "#     Now open System Preferences > Language & Text and change your       #"
+echo "#     input source to 'Noah Dvorak'.                                      #"
+echo "#                                                                         #"
+echo "###########################################################################"
